23ec2f320c879e79e74c4904acd38874ff58dc0f
Jesse Chi <jackjsj@qq.com>
2019-04-10 17:25:43 +0800

    最新活动-无活动

--- src/page/activity/index.vue
+++ src/page/activity/index.vue
@@ -1,5 +1,55 @@
+<!-- 最新活动 -->
 <template>
   <div>
-    <!-- 最新活动 -->
+    <div v-title>最新活动</div>
+    <div v-if="!hasActivity" class="no-activity">
+      <div>
+        <img class="no-activity-img" src="@/assets/icon/最新活动/最新活动.png">
+      </div>
+      <div class="no-activity-desc">
+        <span>敬请期待新活动</span>
+      </div>
+      <div class="no-activity-footer">
+        <p>
+          站点地图<br>
+          37打工网-专为打工者服务的招工招聘信息平台<br>
+        </p>
+        <p>
+          37打工网 37HR.COM ©2012-2017<br>
+          苏州叁柒汇信息技术服务有限公司版权所有<br>
+          苏ICP备17027824号-
+        </p>
+      </div>
+    </div>
   </div>
 </template>
+<script>
+export default {
+  name: '',
+  data() {
+    return {
+      hasActivity: false,
+    }
+  },
+  mounted() {
+    this.$nextTick().then(() => {
+
+    })
+  },
+  methods: {
+
+  },
+  computed: {
+
+  },
+  components: {
+
+  },
+
+}
+
+</script>
+<style scoped lang='scss'>
+@import '~@/styles/activity.scss';
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,19 +3,22 @@ import Router from 'vue-router'
 import Index from '@/page/index/index'
 import LoginRegister from './login'
 import Person from './person'
+import Activity from '@/page/activity/index';
 
 
 Vue.use(Router)
 
 let menu = new Router({
   mode: 'history',
-  routes: [
-    {
-      path: '/',
-      name: '首页',
-      component: Index
-    }
-  ].concat(Person, LoginRegister)
+  routes: [{
+    path: '/',
+    name: '首页',
+    component: Index
+  }, {
+    path: '/activity',
+    name: '最新活动',
+    component: Activity
+  }, ].concat(Person, LoginRegister)
 })
 
 export default menu
