3e4a8b3f8fb6480570c3c015d7e1dd5917322319
midoriruka <sijty@hotmail.com>
2019-04-13 15:30:19 +0800

    企业信息数据接口

--- src/page/recruitment/companyInfo.vue
+++ src/page/recruitment/companyInfo.vue
@@ -49,20 +49,40 @@
         <el-row>
           <el-col :span="18">
             <el-form-item label="企业LOGO">
-              <el-input v-model="fieldData.companyLogo"></el-input>
+              <el-input v-model="imgUrl"></el-input>
             </el-form-item>
           </el-col>
           <el-col :span="6">
+            <!--<input type="text">-->
             <el-upload
-              action="https://jsonplaceholder.typicode.com/posts/"
+              :auto-upload=false
+              :on-change="onchange"
+              action=""
               :limit="1">
               <el-button size="small" style="color: #e6a03c;border-color: #e6a03c;background-color: white;margin-left:0.3rem;">浏览</el-button>
             </el-upload>
           </el-col>
         </el-row>
-        <el-form-item label="所属地区" class="item">
-          <el-input v-model="fieldData.companyContactPhone"></el-input>
-        </el-form-item>
+        <el-row>
+          <el-col :span="9">
+            <el-form-item label="所属地区" label-width="2rem">
+              <el-input v-model="fieldData.companyProvince"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="6">
+            <el-form-item label="省" label-width="0.5rem">
+              <el-input v-model="fieldData.companyCity"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="6">
+            <el-form-item label="市" label-width="0.5rem">
+              <el-input v-model="fieldData.companyArea"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="3" style="margin-top: 0.3rem;color: #969696">
+            <span>区</span>
+          </el-col>
+        </el-row>
         <el-form-item label="详细地址" class="item">
           <el-input v-model="fieldData.companyAddress"></el-input>
         </el-form-item>
@@ -79,15 +99,18 @@
 
 <script>
   import {InduOption, NatureOption, ScaleOption} from '@/assets/addNewCorporate.js'
+  import { MessageBox } from 'mint-ui'
     export default {
       name: "companyInfo",
       data(){
         return{
+          imgUrl:null,
           companyId:null,
           companyIndus:InduOption,
           companyNatures:NatureOption,
           companyScales:ScaleOption,
           fieldData:{
+            companyUserId:JSON.parse(window.localStorage.getItem('userMsg')).users.userId
           }
         }
       },
@@ -110,8 +133,8 @@
           }).then((res) => {
             console.log(res.data.code);
             if (res.code == 200 && res.data.data) {
-              console.log(res.data);
-              this.fieldData = res.data
+              console.log(res.data.data);
+              this.fieldData = res.data.data;
             }
           }).catch((res) => {
             MessageBox({
@@ -121,6 +144,7 @@
           })
         },
         submit(){
+          // this.fieldData.companyUserId=JSON.parse(window.localStorage.getItem('userMsg')).users.userId;
           this.axios({
             method: 'post',
             url: '/api/h5/addEntrust',
@@ -129,18 +153,68 @@
             },
             data: this.fieldData
           }).then((res) => {
-            console.log(res.data);
+            console.log(res.data.code);
             if (res.data.code == 200 && res.data.data) {
+              MessageBox({
+                title: '提示',
+                message: '提交成功！',
+              });
              this.$store.commit('setCompanyId',res.data.data)
             }
           }).catch((res) => {
             MessageBox({
-              title: '小提示',
+              title: '提示',
               message: res.data.msg,
             })
           })
-        }
-      }
+        },
+        onchange(file) {
+          this.imgUrl=file.name;
+          this.fieldData.companyLogo=file.name;
+          console.log(this.fieldData.companyLogo);
+          let fileType=file.name.substring(file.name.lastIndexOf('.'));
+          console.log(fileType);
+          let typeArr = ['.png', '.jpg', '.jpeg'];
+          if (typeArr.indexOf(fileType) > -1) {
+            // 上传文件地址，然后赋值给fileForm.waterFile
+            let reader = new FileReader();
+            reader.readAsDataURL(file.raw);
+            reader.onload = () => {
+              let _base64 = reader.result;
+              this.axios({
+                method: 'post',
+                url: '/api/back/uploadFile',
+                headers: {
+                  'Content-type': 'application/json;charset=UTF-8'
+                },
+                data: {
+                  baseFile: _base64,
+                  baseType: fileType.substring(1)
+                }
+              }).then((res) => {
+                if (res.data.code == 200) {
+                  return;
+                } else {
+                  MessageBox({
+                    title: 'error',
+                    message: '上传失败，请重试',
+                  });
+                }
+              }).catch(() => {
+                MessageBox({
+                  title: 'error',
+                  message: '上传失败，请重试',
+                });
+              })
+            }
+          } else {
+            MessageBox({
+              type: 'error',
+              message: '抱歉，您上传的格式不符合要求，请重新上传'
+            })
+          }
+          },
+        },
     }
 </script>
 
@@ -160,4 +234,33 @@
     font-size: 0.32rem;
   }
 }
+</style>
+<style lang="scss">
+  .el-upload-list{
+    display: none;
+  }
+  .el-input{
+    height:1rem;
+  }
+  .el-input__inner{
+    border: 0;
+  }
+  .el-form-item__content{
+    line-height: 0;
+  }
+  .el-form-item__label{
+    padding-top: 0.3rem;
+    width: 1.067rem;
+    height: 0.307rem;
+    font-family: PingFangSC-Regular;
+    font-size: 0.32rem;
+    line-height: 0.4rem;
+    color: #969696;
+  }
+  .el-col-2{
+    padding-top: 0.3rem;
+  }
+  .el-col-4{
+    padding-top: 0.3rem;
+  }
 </style>
